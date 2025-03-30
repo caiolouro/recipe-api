02dfcfc44b4ed0bbaba2657f93ec5050ded932db
Caio Louro <caio.louro@carta.com>
2025-03-30 18:28:40 -0300

    test: wait for db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,7 +2,21 @@
 To wait for the database to be available before starting the server
 """
 from django.core.management.base import BaseCommand
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for the database to start...')
+        db_started = False
+        while db_started is False:
+            try:
+                self.check(databases=['default'])
+                db_started = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database not started, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database started!'))
--- app/core/tests/management/commands/test_wait_for_db.py
+++ app/core/tests/management/commands/test_wait_for_db.py
@@ -4,18 +4,23 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     def test_when_check_ready(self, patched_check):
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
-    # Beware of patched objects arguments order: first is sleep because it is the "closest" decorator then the outer check one
+    # Beware of patched objects arguments order:
+    # First is sleep because is the "closest" decorator then the outer check
     @patch('time.sleep')
     def test_when_check_delayed(self, patched_sleep, patched_check):
-        # First two calls to check raise Psycopg2Error, 4 raise OperationalError and the last one works
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 4 + [True]
+        # First two calls to check raise Psycopg2Error
+        # Then 4 raise OperationalError
+        # Then the last one works
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 4 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 7)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
