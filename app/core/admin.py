0946e3df82ee3e0b9164a6be6bea009dd75519ad
Caio Louro <caio.louro@carta.com>
2025-04-15 22:06:25 -0300

    feat: add custom UserAdmin listing

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+# Alias because we want our custom user to be called "UserAdmin"
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+
+admin.site.register(models.User, UserAdmin)
