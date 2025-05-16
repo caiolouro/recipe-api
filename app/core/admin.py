2f74312043b25991909c18c702c0e08ec357a9d6
Caio Louro <caio.louro@carta.com>
2025-05-15 21:44:08 -0300

    feat: allow user to be created

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # Alias because we want our custom user to be called "UserAdmin"
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _ # Internationalization shorthand
 
 from core import models
 
@@ -8,6 +9,29 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ["id"]
     list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}), # Tuple containing UI section title (in this case no title) and fields
+        (
+            _("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser")},
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}), # Trailing comma after "last_login" is to Python to recognize it as a tuple
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",), # This is an UI class modifier
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,3 +24,15 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_when_navigate_to_edit_user_returns_ok(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_when_navigate_to_create_user_page_returns_ok(self):
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
